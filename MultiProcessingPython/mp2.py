bd15b830c41d1e358ed77018403ba50e29118b87
Zahid KIZMAZ <mzkizmaz@hotmail.com>
2018-08-10 15:14:45 +0300

    pool first example added

---
+++ MultiProcessingPython/mp2.py
@@ -0,0 +1,10 @@
+from multiprocessing import Pool
+
+def job(num):
+    return num*2
+
+if __name__ == '__main__':
+    p = Pool(processes=20)
+    data = p.map(job, range(10))
+    p.close()
+    print(data)
